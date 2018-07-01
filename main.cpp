5bff6abe80b6548f4e8291b5723679cff25e1f0c
laptop\golg <realgolg@gmail.com>
2018-07-01 19:52:04 +0900

    Base functions added

--- main.cpp
+++ main.cpp
@@ -1,9 +1,44 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
-int main() {
 
+
+// initial data
+
+unsigned short int Dimensions;
+
+vector<vector<int>> initialData;
+
+// tools
+
+int splitDimmension(vector<int> dimensions);
+
+void kdTreeBuilder(vector<vector<int>> data);
+
+void addNode(vector<int> newNode);
+
+void preSort(vector<vector<int>> data);
+
+int main() {
+    initialData.resize(Dimensions);
     cout << "Big Fantastic Start" << endl;
 
     return 0;
+}
+
+int splitDimmension(vector<int> plane) {
+
+    
+
+
+    return 0;
+}
+
+void preSort(vector<vector<int>> data) {
+    for (int i = 0; i < data.size(); ++i) {
+        std::sort(data[i].begin(),data[i].end());
+    }
+
 }
