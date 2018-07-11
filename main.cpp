7702c7344927346b5125c34a03ebfcd7c6fb360c
laptop\golg <realgolg@gmail.com>
2018-07-11 19:15:19 +0900

    Prepared data structure, test sorting

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <stdlib.h>
+#include <random>
+#include <time.h>
 
 using namespace std;
 
@@ -9,7 +12,7 @@ using namespace std;
 
 unsigned short int Dimensions;
 
-vector<vector<int>> initialData;
+vector<vector<int>> initialData(10, vector<int>(Dimensions));
 
 // tools
 
@@ -20,12 +23,37 @@ void kdTreeBuilder(vector<vector<int>> data);
 
 void addNode(vector<int> newNode);
 
-void preSort(vector<vector<int>> data);
+//void preSort(vector<vector<int>> data);
+void preSort();
+
+// Utils
+
+void PrintVector();
 
 int main() {
-    initialData.resize(Dimensions);
+    Dimensions = 4;
+    vector<int> test(8);
+    //--------
+    std::random_device rd;  //Will be used to obtain a seed for the random number engine
+    std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+    std::uniform_int_distribution<> dis(1, 200);
+    //--------
     cout << "Big Fantastic Start" << endl;
 
+    for (int i = 0; i < initialData.size(); ++i) {
+        vector<int> tmp(Dimensions);
+        for (int j = 0; j < tmp.size(); ++j) {
+            tmp.at(j) = dis(gen);
+            //tmp.at(j) = rand_r(time(NULL));
+        }
+        initialData.at(i) = tmp;
+
+    }
+
+
+
+    preSort();
+    PrintVector();
     return 0;
 }
 
@@ -36,10 +64,27 @@ int splitDimmension(int mode, vector<int> plane) {
 
     return 0;
 }
-
+/*
 void preSort(vector<vector<int>> data) {
     for (int i = 0; i < data.size(); ++i) {
         std::sort(data[i].begin(),data[i].end());
     }
 
+}
+*/
+
+void preSort() {
+    for (int i = 0; i < initialData.size(); ++i) {
+        std::sort(initialData[i].begin(),initialData[i].end());
+    }
+}
+
+
+void PrintVector() {
+    for (int i = 0; i < initialData.size(); ++i) {
+        for (int j = 0; j < initialData[j].size(); ++j) {
+            cout << initialData[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
