69b6983a03157f6d40eb6ed778b1c3457607932a
laptop\golg <realgolg@gmail.com>
2018-07-23 19:47:36 +0900

    [ADD] Miltdimensional median calulation + serice Data Struct.

--- main.cpp
+++ main.cpp
@@ -26,7 +26,10 @@ vector<vector<int>> initialData(11, vector<int>(Dimensions));
 
 // service data strcuture
 //vector<vector<int>> column(10, vector<int>(2));
+
 vector<pair<int,int>> column;
+vector<vector<pair<int,int>>> serviceData;
+vector<int> medians;
 // Minimal depth sould correspond to number of dimenstions
 int depth = Dimensions;
 
@@ -48,7 +51,7 @@ bool sortbysec(const pair<int,int> &a, const pair<int,int> &b);
 
 // TODO: Provide alternate splitting methods by setting different option argument in mode
 
-int splitDimmension(int plane);
+int splitDimmension(int plane, int depth);
 
 void kdTreeBuilder(vector<vector<int>> data, int depth);
 
@@ -56,7 +59,9 @@ void addNode(vector<int> newNode);
 
 // void preSort(vector<vector<int>> data);
 void preSort();
-
+void preSort2();
+void Print2();
+void getMedians();
 // Utils
 
 void PrintVector();
@@ -75,7 +80,7 @@ int main() {
     //--------
     std::random_device rd;  //Will be used to obtain a seed for the random number engine
     std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
-    std::uniform_int_distribution<> dis(1, 200);
+    std::uniform_int_distribution<> dis(10, 99);
 
     //--------
     cout << "Big Fantastic Start" << endl;
@@ -90,8 +95,10 @@ int main() {
 
     }
 
-    preSort();
-    PrintVector();
+    //preSort();
+    //PrintVector();
+    preSort2();
+    Print2();
     return 0;
 }
 
@@ -121,6 +128,77 @@ void preSort() {
     std::sort(column.begin(), column.end(), sortbysec);
 
     median = column.at(column.size()/2).second;
+
+}
+
+// Delete this coments
+// vector<vector<pair<int,int>>> serviceData;
+
+void preSort2() {
+    for (int i = 0; i < initialData.size(); ++i) {
+        vector<pair<int,int>> tmp;
+        for (int j = 0; j < Dimensions; ++j) {
+            tmp.emplace_back(make_pair(i, initialData.at(i).at(j))); // first origin Index , second - value
+        }
+        serviceData.emplace_back(tmp);
+    }
+}
+
+// Delete this coments
+// vector<pair<int,int>> column;
+// vector<vector<pair<int,int>>> serviceData;
+
+/*
+ vector<
+    [0] vector<[0]pair<int,int>, [1] pair<int,int>, [2] pair<int,int> ...  [j] pair<int,int> >,
+
+    [1] vector<[0]pair<int,int>, [1] pair<int,int>, [2] pair<int,int> ...  [j] pair<int,int> >,
+
+    [2] vector<[0]pair<int,int>, [1] pair<int,int>, [2] pair<int,int> ...  [j] pair<int,int> >,
+    .
+    .
+    .
+    [i] vector<[0]pair<int,int>, [1] pair<int,int>, [2] pair<int,int> ...  [j] pair<int,int> >
+
+    >
+
+ */
+void getMedians() {
+    for (int i = 0; i < serviceData.size(); ++i) {
+        for (int j = 0; j < serviceData[0].size(); ++j) {
+            std::sort(serviceData[i].begin(), serviceData[i].end(), sortbysec);
+        }
+        // TODO: Put statement as argument of medians.push_back()
+        int med = serviceData.at(serviceData.size()/2)[i].second;
+        medians.push_back(med);
+    }
+}
+
+int splitDimmension(int plane, int depth) {
+
+    for (int space = 0; space < depth; ++space) {
+
+    }
+
+    for (int j = 0; j < initialData.size(); ++j) {
+        column.emplace_back(make_pair(j,initialData[j][0]));
+    }
+    //  remove sortbysec to sort by first element
+    std::sort(column.begin(), column.end(), sortbysec);
+
+    median = column.at(column.size()/2).second;
+
+    return 0; // Index of median
+}
+
+void Print2() {
+    for (int i = 0; i < serviceData.size(); ++i) {
+        cout<< "i: "<< i;
+        for (int j = 0; j < serviceData[j].size(); ++j) {
+            cout << " V: " << serviceData[i][j].second;
+        }
+        cout  << endl;
+    }
 
 }
 
@@ -159,7 +237,7 @@ void BuilderUtil(vector<vector<int>> data, int depth, int dimension) {
 void kdTreeBuilder(vector<vector<int>> data, int depth) {
     int dep = 0;
     int plane = 0;
-    int median = splitDimmension(plane);
+    //int median = splitDimmension(plane);
 
 
 
@@ -167,11 +245,3 @@ void kdTreeBuilder(vector<vector<int>> data, int depth) {
 
 
 
-int splitDimmension(int plane) {
-
-
-
-
-
-    return 0; // Index of median
-}
